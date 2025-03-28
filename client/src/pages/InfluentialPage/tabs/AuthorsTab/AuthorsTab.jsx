53e8a955397072b38f3d8d548299d4aab4bc146d
James Kanin <kaninnoothep@gmail.com>
2025-03-28 00:14:23 -0600

    author tab layout

--- client/src/pages/InfluentialPage/tabs/AuthorsTab/AuthorsTab.jsx
+++ client/src/pages/InfluentialPage/tabs/AuthorsTab/AuthorsTab.jsx
@@ -1,19 +1,95 @@
-import { Typography } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Grid2,
+  List,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 export const AuthorsTab = () => {
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
   return (
-    <>
-      <Typography
-        variant="h5"
+    <Grid2
+      container
+      wrap="no-wrap"
+      sx={{
+        height: "calc(100vh - 280px)",
+      }}
+    >
+      <Grid2
+        size={3}
         sx={{
-          wordWrap: "break-word",
-          marginTop: 1,
-          marginBottom: 4,
-          textTransform: "capitalize",
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
         }}
       >
-        Authors
-      </Typography>
-    </>
+        <Typography
+          variant="h5"
+          sx={{
+            margin: 2,
+          }}
+        >
+          Topics (34)
+        </Typography>
+
+        <Divider />
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            overflowY: "auto",
+          }}
+        >
+          <List
+            sx={{
+              width: "100%",
+              p: 0,
+            }}
+          >
+            {[
+              { label: "Trash", index: 1 },
+              { label: "Spam", index: 2 },
+              { label: "Trash", index: 3 },
+              { label: "Spam", index: 4 },
+              { label: "Trash", index: 5 },
+              { label: "Spam", index: 6 },
+              { label: "Trash", index: 7 },
+              { label: "Spam", index: 8 },
+              { label: "Trash", index: 9 },
+              { label: "Spam", index: 10 },
+              { label: "Trash", index: 11 },
+              { label: "Spam", index: 12 },
+              { label: "Trash", index: 3 },
+              { label: "Spam", index: 14 },
+              { label: "Trash", index: 15 },
+              { label: "Spam", index: 16 },
+            ].map((item, idx) => (
+              <ListItemButton
+                key={idx}
+                selected={selectedIndex === item.index}
+                onClick={(event) => handleListItemClick(event, item.index)}
+              >
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            ))}
+          </List>
+        </Box>
+      </Grid2>
+
+      <Divider orientation="vertical" />
+
+      <Grid2 size={9} padding={2}>
+        Scatterplot here
+      </Grid2>
+    </Grid2>
   );
 };
