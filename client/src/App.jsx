5f6383b90569fd89fa1a101a81b68ad272d56293
James Kanin <kaninnoothep@gmail.com>
2025-04-23 03:59:27 -0600

    comment main, App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+/**
+ * @component App
+ * @description Root application component that provides routing and theming context
+ */
+
 import { BrowserRouter } from "react-router";
 import { Root } from "./pages/Root";
 import { ThemeProvider } from "./context/ThemeProvider/ThemeProvider";
 
+/**
+ * Sets up the core application structure with routing and theme configuration
+ */
 function App() {
   return (
     <BrowserRouter>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,15 @@
+/**
+ * @module main
+ * @description Application entry point that renders the root React component
+ */
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+/**
+ * Mount the App component to the DOM
+ */
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
